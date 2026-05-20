8e2faf91e0524929033bab1e6fc2fa725f739039
Ismael Zanon <smack.otacrazy@gmail.com>
2026-05-20 09:34:44 -0300

    Admin2

--- galeria/models.py
+++ galeria/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 class Fotografia(models.Model):
 
@@ -14,6 +15,8 @@ class Fotografia(models.Model):
     categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
+    publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
 
     def __str__(self):
         return f"Fotografia [nome={self.nome}]"
