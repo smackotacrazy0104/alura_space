47e7cbaa217058bbaea639e9c43ff1ac1d5745db
Ismael Zanon <smack.otacrazy@gmail.com>
2026-05-27 09:49:34 -0300

    persistencia de dados

--- setup/settings.py
+++ setup/settings.py
@@ -125,3 +125,13 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'setup/static'),]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Media files (Uploaded images)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
